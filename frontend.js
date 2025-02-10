1871f08ad52027441fd52c055e69ab97ad8e3f80
Antonio <adelarosariquelme1999@gmail.com>
2025-02-10 12:42:40 +0100

    Añadir función para posicionar personajes aleatoriamente en la pantalla

--- frontend.js
+++ frontend.js
@@ -1,6 +1,26 @@
-// function positionRandom(){
-//     let pantalla=document.getElementById('pantallaInicio'); 
-//     let personaje01=document.getElementById('personaje-01'); 
+function positionRandom(){
+    let personajes=document.querySelectorAll('.personajes'); 
 
-   
-// }
+    
+    let randomIndex=Math.floor(Math.random()*personajes.length); 
+    let personajeSeleccionado=personajes[randomIndex]; 
+
+    personajeSeleccionado.style.display="block"; 
+
+
+    let pantallaJuego=document.getElementById('pantallaJuego'); 
+    let pantallaWidth=pantallaJuego.clientWidth; 
+    let pantallaHeight=pantallaJuego.clientHeight; 
+
+    let personajeWidth= personajeSeleccionado.clientWidth;  
+    let personajeHeight= personajeSeleccionado.clientHeight;
+    
+    let randomX=Math.random()*(pantallaWidth-personajeWidth);
+    let randomY=Math.random()*(pantallaHeight-personajeHeight);
+
+
+    personajeSeleccionado.style.position="absolute";
+    personajeSeleccionado.style.left=randomX+'px'; 
+    personajeSeleccionado.style.top=randomY+'px';
+
+}
